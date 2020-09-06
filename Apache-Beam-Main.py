27165439bc6f17e0ee811a427c153065cc0e0af9
Andy-Lau168 <andylau1993@googlemail.com>
2020-09-06 17:33:55 +0100

    Added some comments

--- Apache-Beam-Main.py
+++ Apache-Beam-Main.py
@@ -9,6 +9,7 @@ import argparse
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
+#Adding argument parser if necessary
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -19,7 +20,7 @@ parser.add_argument(
 parser.add_argument(
       '--output',
       dest='output',
-      default='testA.csv',
+      default='test.csv',
       help='Output file to write results to.')
 known_args, pipeline_args = parser.parse_known_args()
 
@@ -27,7 +28,8 @@ known_args, pipeline_args = parser.parse_known_args()
 class Split(beam.DoFn):
     def process(self, element):
         if len(element.split(",")) == 16:
-            TransID,Price,Date,Postcode,PropertyType,OldNew,Duration,PAON,SAON,Street,Locality,TownCity,District,County,PPDCat,RecordStatus = element.split(",") 
+            TransID,Price,Date,Postcode,PropertyType,OldNew,Duration,PAON,SAON,Street,Locality,TownCity,District,County,PPDCat,RecordStatus = element.split(",")
+            #Only keep columns that we might want
             return [{
                 'TransactionID': TransID,
                 'Date': Date,
@@ -48,7 +50,9 @@ with beam.Pipeline(options=pipeline_options) as p:
     csv_lines = (
             p | beam.io.ReadFromText(known_args.input) 
               | beam.ParDo(Split())
+                # Map Street, Postcode, PAON ad SAON to transactions, date of transaction and the price
               | beam.Map(lambda x: ((x['Street'],x['Postcode'],x['PAON'],x['SAON']),(x['TransactionID'],x['Date'],x['Price'])))
+                # Group by properties
               | beam.GroupByKey()
               | beam.io.WriteToText(known_args.output)
                  )
